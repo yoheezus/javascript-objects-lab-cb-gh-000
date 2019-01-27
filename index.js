0515df168e87dd75a13eae28e3e88b44eb9327a1
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function updateObjectWithKeyAndValue(obj, key, value) {
     var newObj = Object.assign({}, obj, {key: value})
     return newObj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
