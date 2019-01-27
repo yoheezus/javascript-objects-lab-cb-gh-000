a72a96dde02ba1eba03ab01bbc9a1728b39e0361
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:24:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const recipes = {}
 // }
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-    newObj = Object.assign({}, obj, {key: value})
+    var newObj = Object.assign({}, obj, {key: value})
     return newObj
 }
