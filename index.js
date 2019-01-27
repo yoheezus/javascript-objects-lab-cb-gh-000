2cf6670705fbf78b6abaf95d7b6176ede30bdad8
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
     return obj;
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(obj, key, value) {
     return Object.assign({}, obj, {key: value})
 }
