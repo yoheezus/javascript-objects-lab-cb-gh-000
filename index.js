bd554dc895a0511ccea22c8d38c2725c3a33f14c
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(obj, key, value) {
-    obj[key] = value;
-    return obj;
-}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-    var newObj = Object.assign({}, obj, {key: value})
+    var newObj = Object.assign({}, obj, { [key]: value})
     return newObj
 }
 
